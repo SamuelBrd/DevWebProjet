7d6c2e3adb5bb0fbcba8a52b62f0f3ddb5a3acf6
Samuel Briand <samuelbrianddjako@gmail.com>
2021-03-23 23:31:25 +0100

    Résolution du problème d'encodage de password dans userFixtures

--- src/DataFixtures/EvenementFixtures.php
+++ src/DataFixtures/EvenementFixtures.php
@@ -28,9 +28,33 @@ class EvenementFixtures extends Fixture
         ->setTypeEvent('GENEPI Nantes')
         ->setDateEvent(\DateTime::createFromFormat('d-m-Y', $date2))
         ->setDureeEvent(1);
+
+        $event3 = new Evenement();
+        $date3 = "01-01-2021";
+        $event3->setNameEvent('Evenement Passe')
+        ->setTypeEvent('GENEPI Nantes')
+        ->setDateEvent(\DateTime::createFromFormat('d-m-Y', $date3))
+        ->setDureeEvent(2);
+
+        $event4 = new Evenement();
+        $date4 = "11-08-2021";
+        $event4->setNameEvent('Test')
+        ->setTypeEvent('InterAsso Nantes')
+        ->setDateEvent(\DateTime::createFromFormat('d-m-Y', $date4))
+        ->setDureeEvent(1);
+
+        $event5 = new Evenement();
+        $date5 = "01-01-2021";
+        $event5->setNameEvent('Test2')
+        ->setTypeEvent('InterAsso Nantes')
+        ->setDateEvent(\DateTime::createFromFormat('d-m-Y', $date5))
+        ->setDureeEvent(1);
         
         $manager->persist($event1);
         $manager->persist($event2);
+        $manager->persist($event3);
+        $manager->persist($event4);
+        $manager->persist($event5);
 
         $manager->flush();
     }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,9 +5,18 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     /**
      * @param ObjectManager $manager
      *
@@ -17,25 +26,50 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
     {
         $admin1 = new User();
         $roles[] = 'ROLE_ADMIN';
+        $password1 = $this->encoder->encodePassword($admin1, 'azertyuiop');
         $admin1->setEmail('admin1@gmail.com')
         ->setRoles($roles)
-        ->setPassword('azertyuiop')
+        ->setPassword($password1)
         ->setNom('Admin1')
         ->setPseudo('Admin1')
         ->setPhoneNumber('0615489562')
         ->setCommuName('Hashtag Info');
 
         $admin2 = new User();
+        $password2 = $this->encoder->encodePassword($admin2, 'azertyuiop');
         $admin2->setEmail('admin2@gmail.com')
         ->setRoles($roles)
-        ->setPassword('azertyuiop')
+        ->setPassword($password2)
         ->setNom('Admin2')
         ->setPseudo('Admin2')
         ->setPhoneNumber('0615847851')
         ->setCommuName('ARTicule Nantes');
 
+        $admin3 = new User();
+        $password3 = $this->encoder->encodePassword($admin3, 'azertyuiop');
+        $admin3->setEmail('admin3@gmail.com')
+        ->setRoles($roles)
+        ->setPassword($password3)
+        ->setNom('Admin3')
+        ->setPseudo('Admin3')
+        ->setPhoneNumber('0615762484')
+        ->setCommuName('GENEPI Nantes');
+
+        $user1 = new User();
+        $rolesU[] = 'ROLE_MEMBER';
+        $password4 = $this->encoder->encodePassword($user1, 'azertyuiop');
+        $user1->setEmail('user1@gmail.com')
+        ->setRoles($rolesU)
+        ->setPassword($password4)
+        ->setNom('User1')
+        ->setPseudo('User1')
+        ->setPhoneNumber('0654952659')
+        ->setCommuName('GENEPI Nantes');
+
         $manager->persist($admin1);
         $manager->persist($admin2);
+        $manager->persist($admin3);
+        $manager->persist($user1);
 
         $manager->flush();
     }
